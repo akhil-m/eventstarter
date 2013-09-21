02315a8eb3e2ea170d4a05aeb2ed9b50330e1352
Akhil Mallavarapu <akhil1710@gmail.com>
2013-09-21 03:37:16 -0400

    added some shit

---
+++ django/et/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Event(models.Model):
+	name = models.CharField(max_length=50)
+	description = models.TextField()
+	goal = models.IntegerField()
+	current = models.IntegerField()
+	deadline = models.DateTimeField()
+	created_by = models.ForeignKey(User, unique=True)
+
+class UserProfile(models.Model):
+	user = models.ForeignKey(User, unique=True)
+	email = models.EmailField()
+	events = models.ManyToManyField(Event)
